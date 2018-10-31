3647b045f87b94cf5cd6a1dbc1ea957952514c7c
= <janepress940214@outlook.com>
2018-10-31 03:48:35 -0700

    none jsx-js

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,17 +1,14 @@
 const models = require('../models');
-var React = require('react');
-var ReactDOMServer = require('react-dom/server');
-var Component = require ('../views/HelloWorld.jsx');
 
 exports.index = (req, res) => {
-    // res.json([{
-    //     id: 1,
-    //     name: "Hiccup",
-    //     password: 'hiccup'
-    //   }, {
-    //     id: 2,
-    //     name: "King Arthur",
-    //     password: 'king-arthur'
-    //   }]);
+    res.json([{
+        id: 1,
+        name: "Hiccup",
+        password: 'hiccup'
+      }, {
+        id: 2,
+        name: "King Arthur",
+        password: 'king-arthur'
+      }]);
     
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,53 @@
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
 module.exports = {
-    entry: './views/index.jsx',
-    output: {
-        path:__dirname + '/public/javascripts',
-        filename: 'bundle.js',
-    },
-    module: {
-        // loaders: [
-        //     {
-        //         exclude: /(node_modules)/,
-        //         loaders: babel,
-        //         query: {
-        //             presets: ['es2015', 'react']
-        //         }
-        //     }
-        // ],
-        rules: [
+  module: {
+    //   entry: "./src/index.js",
+    //   output: {
+    //     path: path.resolve('dist'),
+    //     filename: 'bundled.js'
+    //   },
+    rules: [
+      {
+        test: /\.js|jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+          test: /\.css$/,
+          use: [
+            "style-loader",
             {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
+                loader: "css-loader",
+                options: {
+                    modules: true,
+                    importLoaders: 1,
+                    localIdentName: "[name]_[local]_[hash:base64]",
+                    sourceMap: true,
+                    minimize: true
                 }
             }
         ]
-    },
-    watch: true
+      },
+      {
+          test: /\.jsx$/,
+          use: {
+              loader: "jsx-loader",
+              options: {
+                  modules: true,
+              }
+          }
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
 };
